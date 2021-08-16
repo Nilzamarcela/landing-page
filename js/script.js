cb1e3f9715cdc1e25b434993b569e2778844ea14
Nilzamarcela <nilzare@gmail.com>
2021-08-15 20:42:31 -0400

    imagenes

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function validar(){
+
+alert("Hello! I am an alert box!!");
+/*
+    var nombre, email, mensaje;
+    nombre = document.getElementById("nombre").value;
+    email = document.getElementById("email").value;
+    mensaje = document.getElementById("mensaje").value;
+    
+        
+    validamail = /\w+@\w+\.+[a-z]/;  // declaramos una expresion regular para validar el campo correo electrónico
+
+
+    if (nombre === "" || email === "" || Mensaje === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (nombre.length>30) {
+        alert("El nombre es muy largo");
+        return false;
+    }
+    Alert('hola');
+    else if (email.length>30){
+        alert("El correo es muy largo");
+        return false;
+    }
+
+    
+    else if (mensaje.length>250){
+        alert("El maximo de caracteres es de 250");
+        return false;
+        
+    }
+     else if (!validamail.test(email)){
+        alert("El correo no es válido");
+        return false;
+    }
+
+    /* 
+  
+    var saludo = "Bienvenido " + nombre + apellido + "!!!. Gracias por su mensaje nos contactaremos con usted a la brevedad" ;
+    
+    alert(saludo);*/
+*/
+}
